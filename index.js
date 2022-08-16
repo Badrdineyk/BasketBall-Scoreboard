5e623ca19dd57fe644d03c1055db1ecfed1e5a36
Badr <110286098+Badrdineyk@users.noreply.github.com>
2022-08-16 01:57:54 -0700

    Commit Initial Files

---
+++ index.js
@@ -0,0 +1,27 @@
+let finalResultHome = document.getElementById("resulthome")
+let finalResultGuest = document.getElementById("resultguest")
+
+
+function btnh1() {
+    finalResultHome.textContent = parseInt(finalResultHome.textContent) + 1
+}
+
+function btnh2() {
+    finalResultHome.textContent = parseInt(finalResultHome.textContent) + 2
+}
+
+function btnh3() {
+    finalResultHome.textContent = parseInt(finalResultHome.textContent) + 3
+}
+
+function btng1() {
+    finalResultGuest.textContent = parseInt(finalResultGuest.textContent) + 1
+}
+
+function btng2() {
+    finalResultGuest.textContent = parseInt(finalResultGuest.textContent) + 2
+}
+
+function btng3() {
+    finalResultGuest.textContent = parseInt(finalResultGuest.textContent) + 3
+}
